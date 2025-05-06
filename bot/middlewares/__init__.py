2910f1abef5a6ced5f567a1a3b7d52b5e9f6778e
TehQueen <iightqueeh@gmail.com>
2025-05-07 02:27:11 +0300

    feat: add module documentation and define public symbols for middlewares

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -1,8 +1,16 @@
-from .throttle import ThrottleMiddleware
+"""
+This module initializes and exports the middlewares used in the application.
+Exports:
+    __middlewares__ (list): A list of middleware instances to be used in the application.
+        - ThrottleMiddleware: Middleware that enforces a throttle time to limit the rate of requests.
+Attributes:
+    __all__ (list): Specifies the public symbols that this module exports.
+"""
+from bot.middlewares.throttle import ThrottleMiddleware
 
+
+__all__ = ["__middlewares__"]
 
 __middlewares__ = [
     ThrottleMiddleware(throttle_time=384),
 ]
-
-__all__ = []
