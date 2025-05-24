ebb66884b8ba4038d7a10fd4cb6835bde8de7b63
TehQueen <iightqueeh@gmail.com>
2025-05-24 15:10:10 +0300

    feat(personal): implemented handler templates for basic commands
    - Added the following handlers:
        * /start
        * /settings
        * /help

--- bot/handlers/personal/messages.py
+++ bot/handlers/personal/messages.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from aiogram.utils.i18n import I18n, gettext as _
 
@@ -12,9 +12,15 @@ from bot.handlers.personal.keyboards import cmd_start_kb
 router = Router(name=__name__) 
 
 @router.message(CommandStart())
-async def cmd_start(
-    message: Message, user: User, i18n: I18n
-) -> Any:
-    await message.answer(_("cmd-start").format(
+async def cmd_start(message: Message, user: User, i18n: I18n) -> Any:
+    return await message.answer(_("cmd-start").format(
         name=user.full_name
     ), reply_markup=cmd_start_kb(i18n))
+
+@router.message(Command("settings"))
+async def cmd_settings(message: Message) -> Any:
+    return await message.answer(_("cmd_settings"))
+
+@router.message(Command("help"))
+async def cmd_help(message: Message, i18n: I18n) -> Any:
+    return await message.answer(_("cmd-help"), reply_markup=cmd_start_kb(i18n))
