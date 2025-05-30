14ef39ac595136ffee2268d30de16556c980978b
TehQueen <iightqueeh@gmail.com>
2025-05-30 21:34:50 +0300

    feat(personal): some changes were made to the /start command handler
    - Reworked keyboard objects:
        * added ReplyKeyboard
    - Removed all callbacks
    - Updated localization files
        * Added Russian localization

--- bot/handlers/personal/keyboards.py
+++ bot/handlers/personal/keyboards.py
@@ -1,21 +1,32 @@
 """
 Module: personal.keyboards
-Description: Contains lazy-initialized inline keyboards for personal (1:1) bot interactions.
+Description: Contains lazy-initialized inline and reply keyboards for personal bot interactions.
 All keyboards are cached for 24 hours (86400 seconds) to optimize resource usage.
 """
 
+from aiogram.types import LoginUrl, WebAppInfo
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from bot.core import settings
 from bot.utils.keyboards import LazyKeyboard
 
 
 # Creates inline keyboard for the start command with localization support
-cmd_start_kb = LazyKeyboard(
+cmd_start_kb_rp = LazyKeyboard(
+    lambda gettext: ReplyKeyboardMarkup(keyboard=[
+        [
+            KeyboardButton(text=gettext('📢')),
+            KeyboardButton(text=gettext('🧙‍♂️')),
+        ],
+        [KeyboardButton(text=gettext('⚙️'), web_app=WebAppInfo(url=settings.WEBAPP_EP))],
+    ], resize_keyboard=True, one_time_keyboard=True), cache_expire=86400
+)
+
+cmd_start_kb_il = LazyKeyboard(
     lambda gettext: InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text=gettext('⚙️'), callback_data="@settings"),
-            InlineKeyboardButton(text=gettext('📢'), callback_data="@audience"),
+            InlineKeyboardButton(text=gettext('⚙️'), login_url=LoginUrl(url=settings.WEBAPP_EP)),
         ],
-        [InlineKeyboardButton(text=gettext('📈'), callback_data="@analytics")]
     ]), cache_expire=86400
 )
--- bot/handlers/personal/messages.py
+++ bot/handlers/personal/messages.py
@@ -6,21 +6,15 @@ from aiogram.utils.i18n import I18n, gettext as _
 from typing import Any
 
 from bot.database.models import User
-from bot.handlers.personal.keyboards import cmd_start_kb
+from bot.handlers.personal.keyboards import cmd_start_kb_il, cmd_start_kb_rp
 
 
 router = Router(name=__name__) 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message, user: User, i18n: I18n) -> Any:
+    stk = "CAACAgIAAxkBAAIQXWg58j9hOdB1AYC9Juma7XvrgVRFAALsUwACgHSQS7FGQPxX0N6VNgQ"
+    await message.answer_sticker(stk, reply_markup=cmd_start_kb_rp(i18n))
     return await message.answer(_("cmd-start").format(
         name=user.full_name
-    ), reply_markup=cmd_start_kb(i18n))
-
-@router.message(Command("settings"))
-async def cmd_settings(message: Message) -> Any:
-    return await message.answer(_("cmd_settings"))
-
-@router.message(Command("help"))
-async def cmd_help(message: Message, i18n: I18n) -> Any:
-    return await message.answer(_("cmd-help"), reply_markup=cmd_start_kb(i18n))
+    ), reply_markup=cmd_start_kb_il(i18n))
