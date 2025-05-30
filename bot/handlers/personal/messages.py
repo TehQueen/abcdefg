f3fea0de4d7eb483653849e14f0dc78404964a30
TehQueen <iightqueeh@gmail.com>
2025-05-30 22:33:26 +0300

    feat(personal): Added request handling via hot reply
    - Added hot query handling
    - Updated localization files

--- bot/handlers/personal/messages.py
+++ bot/handlers/personal/messages.py
@@ -1,11 +1,14 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-from aiogram.utils.i18n import I18n, gettext as _
+from aiogram.utils.i18n import I18n, gettext as _, lazy_gettext as __
+
+from aiogram.utils.keyboard import KeyboardBuilder
 
 from typing import Any
 
 from bot.database.models import User
+from bot.handlers.personal.filters import I18nFilter
 from bot.handlers.personal.keyboards import cmd_start_kb_il, cmd_start_kb_rp
 
 
@@ -18,3 +21,11 @@ async def cmd_start(message: Message, user: User, i18n: I18n) -> Any:
     return await message.answer(_("cmd-start").format(
         name=user.full_name
     ), reply_markup=cmd_start_kb_il(i18n))
+
+@router.message(I18nFilter('📢'))
+async def rsp_channels(message: Message) -> Any:
+    return await message.answer(_("my-channels"))
+
+@router.message(I18nFilter('🧙‍♂️'))
+async def rsp_groups(message: Message) -> Any:
+    return await message.answer(_("my-groups"))
